d7e3d4a068c5b1a291d3bc3e46f1818c6f420b40
Christian G. Fritsch <guilherme@opiniaoearte.com.br>
2016-07-11 14:40:42 -0300

    Commit inicial

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name imageuploadApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the imageuploadApp
+ */
+angular.module('imageuploadApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
