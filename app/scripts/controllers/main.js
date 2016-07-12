5ce8338c6c1732d10fdbc58766e59a9aa8cc9f62
Christian G. Fritsch <guilherme@opiniaoearte.com.br>
2016-07-12 17:37:39 -0300

    Melhorada a aplicação da extensão

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,25 +9,28 @@
  */
 angular.module('imageuploadApp')
   .controller('MainCtrl', function ($scope, FileUploader, ngToast, $window) {
+    $scope.model = {
+      uploaded: []
+    };
     $scope.uploader = new FileUploader({
-      url: 'http://localhost:9255/upload',
+      url: 'http://localhost:9255/api/v1/media',
       removeAfterUpload: true
     });
     $scope.dragAndDrop = new FileUploader({
-      url: 'http://localhost:9255/upload',
+      url: 'http://localhost:9255/api/v1/media',
       removeAfterUpload: true
     });
-    $scope.dragAndDrop.onSuccessItem = function(item) { //item, reponse, status, header
+    $scope.dragAndDrop.onSuccessItem = function(item, response, status, header) { //item, reponse, status, header
+      console.log(item, response, status, header);
       ngToast.dismiss();
       ngToast.create({
         className: 'success',
         content: '<span class="fa fa-check"></span> ' + item.file.name + ' successful uploaded!',
         timeout: 4000
       });
+      // $scope.model.uploaded.push()
     };
-    $scope.dragAndDrop.onAfterAddingFile = function(item) {
-      console.log(item);
-    };
-
-
+    // $scope.dragAndDrop.onAfterAddingFile = function(item) {
+    //   console.log(item);
+    // };
   });
--- server.js
+++ server.js
@@ -1,36 +1,96 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var uuid = require('node-uuid');
 var fs = require('fs');
-var formidable = require('formidable');
 var multer = require('multer');
 var app = express();
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-var storage =   multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './uploads');
-  },
-  filename: function (req, file, callback) {
-    callback(null, Date.now() + '-' + file.originalname);
+app.use(function (req, res, next) {
+  res.url = function (path) {
+    return [req.protocol+'://'+req.get('host')].concat(path).join('/');
   }
+  next();
 });
 
-var upload = multer({ storage : storage}).single('file');
 
-app.post('/upload',function(req,res){
-    
-    upload(req,res,function(err) {
-      console.log(arguments);
-        if(err) {
-            return res.end("Error uploading file.");
+// var storage =   multer.diskStorage({
+//   destination: function (req, file, callback) {
+//     callback(null, './uploads');
+//   },
+//   filename: function (req, file, callback) {
+//     callback(null, Date.now() + '-' + file.originalname);
+//   }
+// });
+
+// var upload = multer({ storage : storage}).single('file');
+
+var extractExtension = function(name) {
+  return name.split('.').reverse()[0];
+}
+app.post('/api/v1/media', function(req, res){
+    var id = uuid.v4();
+    var storage = multer.diskStorage({
+      destination: function(r, f, callback) {
+        callback(null, './uploads');
+      },
+      filename: function(r, file, callback) {
+        switch (file.mimetype) {
+          case 'image/jpeg': {
+            callback(null, `${id}.jpeg`);
+            break;
+          }
+          case 'image/png': {
+            callback(null, `${id}.png`);
+            break;
+          }
+          case 'image/gif': {
+            callback(null, `${id}.gif`);
+            break;
+          }
+          default: {
+            callback(true);
+            break;
+          }
         }
-        res.end("File is uploaded");
+
+      }
+    });
+    var upload = multer({ storage : storage}).single('file');
+    upload(req,res,function(err) {
+        if(err) {
+            return res.status(422).end();
+        }
+        res.location(res.url(['media', id]));
+        res.status(201).end();
     });
 });
+
+app.get('/api/v1/media/:id', function( req, res) {
+
+  var options = {
+    root: __dirname + '/uploads'
+  };
+  var id = req.params.id;
+  var callback = function(error) {
+    if (error) { res.sendStatus(404); }
+  };
+  if (req.accepts('jpeg')) {
+    res.contentType('image/jpeg');
+    res.sendFile(`${id}.jpeg`, options, callback);
+  } else if (req.accepts('png')) {
+    res.contentType('image/png');
+    res.sendFile(`${id}.png`, options, callback);
+  } else {
+    res.sendStatus(415);
+  }
+})
 
 // app.post('/upload', function(req, res) {
 //   var form = new formidable.IncomingForm();
@@ -54,5 +114,5 @@ app.get('/digaOla', function(req, res) {
   res.send("Olá!").end();
 })
 app.listen(9255, function() {
-  console.log('Let\'s do it on 9255...');
+  console.log('Let\'s do it on 9255... ');
 });
